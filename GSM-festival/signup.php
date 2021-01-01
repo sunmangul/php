577ba287fc158af3258343809114a5d216e96af3
sunmangul <69895365+sunmangul@users.noreply.github.com>
2021-01-01 17:24:05 +0900

    Update signup.php

--- GSM-festival/signup.php
+++ GSM-festival/signup.php
@@ -2,7 +2,7 @@
 
     $mysql_hostname = '127.0.0.1';
     $mysql_username = 'root';
-    $mysql_password = 'shaki0920';
+    $mysql_password = 'password';
     $mysql_database = 'member';
     $mysql_port = '3306';
 
